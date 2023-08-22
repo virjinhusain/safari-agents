a0872e024eeb71880884597f417fc1e978e9aa34
edwinhati <edwinhati@outlook.com>
2023-08-22 20:00:03 +0800

    Changes on TableContainer

--- src/components/TableContainer.jsx
+++ src/components/TableContainer.jsx
@@ -25,15 +25,44 @@ export default function TableContainer({
           {data.map((row) => (
             // eslint-disable-next-line react/jsx-key
             <TableRow>
-              <TableCell>{row.idAgent}</TableCell>
-              <TableCell>{row.tagRegion}</TableCell>
-              <TableCell>{row.travelAgent}</TableCell>
-              <TableCell>{row.contactPerson}</TableCell>
-              <TableCell>{row.phoneNumber}</TableCell>
-              <ActionsTableCell
+              <TableCell
                 data={row}
                 setData={setData}
                 setOpenReadModal={setOpenReadModal}
+              >
+                {row.idAgent}
+              </TableCell>
+              <TableCell
+                data={row}
+                setData={setData}
+                setOpenReadModal={setOpenReadModal}
+              >
+                {row.tagRegion}
+              </TableCell>
+              <TableCell
+                data={row}
+                setData={setData}
+                setOpenReadModal={setOpenReadModal}
+              >
+                {row.travelAgent}
+              </TableCell>
+              <TableCell
+                data={row}
+                setData={setData}
+                setOpenReadModal={setOpenReadModal}
+              >
+                {row.contactPerson}
+              </TableCell>
+              <TableCell
+                data={row}
+                setData={setData}
+                setOpenReadModal={setOpenReadModal}
+              >
+                {row.phoneNumber}
+              </TableCell>
+              <ActionsTableCell
+                data={row}
+                setData={setData}
                 setOpenDeleteModal={setOpenDeleteModal}
               />
             </TableRow>
@@ -52,8 +81,18 @@ function TableRow({ children }) {
   return <tr className="border-b dark:border-gray-700">{children}</tr>;
 }
 
-function TableCell({ children }) {
-  return <td className="px-2 py-3">{children}</td>;
+function TableCell({ children, data, setData, setOpenReadModal }) {
+  return (
+    <td
+      className="px-2 py-3"
+      onClick={() => {
+        setData(data);
+        setOpenReadModal(true);
+      }}
+    >
+      {children}
+    </td>
+  );
 }
 
 function ActionsTableHeaderCell() {
@@ -64,63 +103,27 @@ function ActionsTableHeaderCell() {
   );
 }
 
-function ActionsTableCell({
-  data,
-  setData,
-  setOpenReadModal,
-  setOpenDeleteModal,
-}) {
-  const [showActions, setShowActions] = useState(false);
-
+function ActionsTableCell({ data, setData, setOpenDeleteModal }) {
   return (
     <td className="px-4 py-3 relative text-right">
       <button
-        className="text-gray-500 hover:text-gray-800 focus:outline-none"
         type="button"
+        className="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-gray-700 dark:text-gray-200"
+      >
+        <i className="fas fa-edit mr-2"></i>
+        Edit
+      </button>
+      <button
+        type="button"
+        className="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 text-red-500 dark:hover:text-red-400"
         onClick={() => {
-          setShowActions(!showActions);
           setData(data);
+          setOpenDeleteModal(true);
         }}
       >
-        <i className="fas fa-ellipsis-v"></i>
+        <i className="fas fa-trash-alt mr-2"></i>
+        Delete
       </button>
-      {showActions && (
-        <div className="absolute right-0 mt-2 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-          <ul className="py-1 text-sm">
-            <li>
-              <button
-                type="button"
-                className="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-gray-700 dark:text-gray-200"
-              >
-                <i className="fas fa-edit mr-2"></i>
-                Edit
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                className="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-gray-700 dark:text-gray-200"
-                onClick={() => {
-                  setOpenReadModal(true);
-                }}
-              >
-                <i className="fas fa-eye mr-2"></i>
-                View details
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                className="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 text-red-500 dark:hover:text-red-400"
-                onClick={() => setOpenDeleteModal(true)}
-              >
-                <i className="fas fa-trash-alt mr-2"></i>
-                Delete
-              </button>
-            </li>
-          </ul>
-        </div>
-      )}
     </td>
   );
 }
