a9212c4551788fe2a9bca3ef81fbbab221655206
edwinhati <edwinhati@outlook.com>
2023-08-29 10:35:46 +0800

    Update on TableContainer

--- src/components/TableContainer.jsx
+++ src/components/TableContainer.jsx
@@ -10,11 +10,11 @@ export default function TableContainer({
   setOpenDeleteModal,
 }) {
   return (
-    <>
-      <table className="w-full text-xs text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <div className="overflow-x-auto">
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
           <tr>
-            <TableHeaderCell>ID Agents</TableHeaderCell>
+            {/* <TableHeaderCell>ID Agents</TableHeaderCell> */}
             <TableHeaderCell>Tag Region</TableHeaderCell>
             <TableHeaderCell>Travel Agents</TableHeaderCell>
             <TableHeaderCell>Contact Person</TableHeaderCell>
@@ -24,15 +24,14 @@ export default function TableContainer({
         </thead>
         <tbody>
           {data.map((row) => (
-            // eslint-disable-next-line react/jsx-key
-            <TableRow>
-              <TableCell
+            <TableRow key={row.idAgent}>
+              {/* <TableCell
                 data={row}
                 setData={setData}
                 setOpenReadModal={setOpenReadModal}
               >
                 {row.idAgent}
-              </TableCell>
+              </TableCell> */}
               <TableCell
                 data={row}
                 setData={setData}
@@ -71,37 +70,49 @@ export default function TableContainer({
           ))}
         </tbody>
       </table>
-    </>
+    </div>
   );
 }
 
 function TableHeaderCell({ children }) {
-  return <th className="px-3 py-2">{children}</th>;
+  return (
+    <th
+      scope="col"
+      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+    >
+      {children}
+    </th>
+  );
 }
 
 function TableRow({ children }) {
-  return <tr className="border-b dark:border-gray-700">{children}</tr>;
+  return <tr className="bg-white divide-y divide-gray-200">{children}</tr>;
 }
 
 function TableCell({ children, data, setData, setOpenReadModal }) {
   return (
-    <td
-      className="px-2 py-3"
-      onClick={() => {
-        setData(data);
-        setOpenReadModal(true);
-      }}
-    >
-      {children}
+    <td className="px-6 py-4 whitespace-nowrap">
+      <div
+        className="text-sm text-gray-900"
+        onClick={() => {
+          setData(data);
+          setOpenReadModal(true);
+        }}
+      >
+        {children}
+      </div>
     </td>
   );
 }
 
 function ActionsTableHeaderCell() {
   return (
-    <TableHeaderCell>
-      <span className="sr-only">Actions</span>
-    </TableHeaderCell>
+    <th
+      scope="col"
+      className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+    >
+      Actions
+    </th>
   );
 }
 
@@ -112,29 +123,27 @@ function ActionsTableCell({
   setOpenDeleteModal,
 }) {
   return (
-    <div className="flex flex-row justify-center items-center">
+    <td className="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
       <button
         type="button"
-        className="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-gray-700 dark:text-gray-200"
+        className="text-indigo-600 hover:text-indigo-900"
         onClick={() => {
           setData(data);
           setOpenUpdateModal(true);
         }}
       >
-        <i className="fas fa-edit mr-2"></i>
         Edit
       </button>
       <button
         type="button"
-        className="flex w-full items-center py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 text-red-500 dark:hover:text-red-400"
+        className="text-red-600 hover:text-red-900 ml-2"
         onClick={() => {
           setData(data);
           setOpenDeleteModal(true);
         }}
       >
-        <i className="fas fa-trash-alt mr-2"></i>
         Delete
       </button>
-    </div>
+    </td>
   );
 }
